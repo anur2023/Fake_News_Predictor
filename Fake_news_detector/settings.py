686c2d34dff7d382b826a080b3c944c6c1cab9a8
Anuruddh <anuruddh209401@gmail.com>
2025-02-18 22:54:32 +0530

    Configure STATIC_ROOT for static files

--- Fake_news_detector/settings.py
+++ Fake_news_detector/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,8 @@ ALLOWED_HOSTS = ['Fake_News_Detector_by_AnuruddhBajpai.onrender.com']
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
